25e91b23dc741e0d8ca30c991fb15e9fadc5685e
Waffle <75454793+maidswithguns@users.noreply.github.com>
2025-04-12 01:07:01 +0900

    12/4/2025

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="center-align">Welcome to My App</h1>
+    </div>
+  );
+}
+
+export default App;
